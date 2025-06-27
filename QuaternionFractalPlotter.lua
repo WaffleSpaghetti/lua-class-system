b3c094d2c949453de52f787820977a466fb67f42
WaffleSpaghetti <135555461+WaffleSpaghetti@users.noreply.github.com>
2025-06-27 21:34:08 +0200

    Update QuaternionFractalPlotter.lua

--- QuaternionFractalPlotter.lua
+++ QuaternionFractalPlotter.lua
@@ -1,8 +1,18 @@
 local Quaternion = require(game.ServerScriptService.QuaternionClass)
 local Q, R, I, toQuaternion, QfromAxisAngle, qexp, qlog = Quaternion.getHelpers()
+--[[
+local function F(z, c)
+	return z^2+c
+end
+--]]
+---[[
+local function F(z, c)
+	return Q(math.abs(z.R),math.abs(z.I),math.abs(z.J),math.abs(z.K))^2 + c -- burning ship fractal function, for mandelbrot use the commented out one
+end
+]]
 
 local t=0
-local res = 0.2
+local res = 0.1
 for z = -2, 2, res do
 	for y = -2, 2, res do
 		for x = -2, 2, res do
@@ -11,7 +21,7 @@ for z = -2, 2, res do
 			local Z = Q(0, 0, 0, 0)
 			local max = 100
 			for i = 1, 100 do
-				Z = Z * Z + C
+				Z = F(Z,C)
 				if Z.abs > 2 then
 					max = i
 					break
@@ -24,7 +34,7 @@ for z = -2, 2, res do
 			local part = Instance.new("Part")
 			part.Anchored = true
 			part.Size = Vector3.one * res * 10
-			part.Position = Vector3.new(x * 10, y * 10 + 10, z * 10)
+			part.Position = Vector3.new(x * 10, y * 10 + 20, z * 10 - 45)
 			local g = max / 100
 			part.Color = Color3.new(g, g, g)
 			part.Transparency = 1 - g
